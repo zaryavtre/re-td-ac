4a2fe62238f2b07e3b30a0e4e15acc8e5cae9f13
zaryavtre <von.zaryavtre@gmail.com>
2023-11-25 01:51:00 +0000

    React + Vite installation

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <h1 className='font-sans text-4xl text-blue-500 font-bold'>RE-AC-TD</h1>
+      </div>
+      <Card 
+      />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+export default function Card(props) {
+    return (
+        <>
+            <div className="px-8 py-6 bg-blue-500/30 rounded-lg">
+                <p>This is a card</p>
+            </div>
+        </>
+    )
+}
