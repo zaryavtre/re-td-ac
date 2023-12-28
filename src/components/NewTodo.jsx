f561c6d2ab32a77964fc2797c20b33038cb9b393
zaryavtre <von.zaryavtre@gmail.com>
2023-12-28 00:39:51 +0000

    update: added checkbox todo

--- src/components/NewTodo.jsx
+++ src/components/NewTodo.jsx
@@ -4,8 +4,12 @@ export default function NewTodo({todoTitle}) {
   console.log(todoTitle)
     return (
       <>
-          {todoTitle.map((todo) => <div className="bg-blue-700 rounded-2xl p-4 flex m-2" id={todo.id} key={todo.id}>
-            <p className="text-white">{todo.title}</p>
+          {todoTitle.map((todo) => <div className="bg-blue-700 rounded-2xl p-4 flex justify-between m-2" id={todo.id} key={todo.id}>
+            <div className="flex row-auto gap-3">
+              <input type="checkbox" id="completedTodo" />
+              <p className="text-white">{todo.title}</p>
+            </div>
+            <div id="removeTodo">X</div>
           </div>)}
       </>
     );
