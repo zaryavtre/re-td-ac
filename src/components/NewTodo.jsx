cd62d2c6281c659b6517369fb52c3ada914fe399
Stéphane <von.zaryavtre@gmail.com>
2024-01-11 23:40:16 +0000

    update: added opacity props for new todo

--- src/components/NewTodo.jsx
+++ src/components/NewTodo.jsx
@@ -3,7 +3,7 @@
 export default function NewTodo({todoTitle, completed}) {
     return (
       <>
-          {todoTitle.map((todo) => <div className="bg-blue-700 rounded-2xl p-4 flex justify-between m-2" id={todo.id} key={todo.id}>
+          {todoTitle.map((todo) => <div style={{opacity: todo.completed ? '50%' : '100%'}} className="bg-blue-700 rounded-2xl p-4 flex justify-between m-2" id={todo.id} key={todo.id}>
             <div className="flex row-auto gap-3">
               <input type="checkbox" data-complete={todo.id} onChange={() => completed(todo.id)} value={todo.completed} />
               <p className="text-white">{todo.title}</p>
